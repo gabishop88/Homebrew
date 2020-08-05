05a547032db7cbc27314491a66b0be02feb3cd5e
Graham Bishop <gabishop15@gmail.com>
2020-08-04 23:51:01 -0500

    Connected to server using socket.io and localtunnel for hosting.

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,9 +1,19 @@
+var socket;
+
 function setup() {
+  socket = io.connect('https://homebrew.serverless.social/');
   createCanvas(400, 400);
   background(51);
+
+  console.log("Hello! Welcome to the Homebrew");
 
   var p = document.getElementById('test-json'); // For testing
 }
 
 function draw() {
+}
+
+function logButton() {
+  var data = "Pressed the button";
+  socket.emit('button', data);
 }
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Starting server");
+var express = require('express');
+var app = express();
+var server = app.listen(3000); //Start server on localhost:3000
+// var server = require('http').createServer(app);
+
+app.use(express.static('public')); //Give the contents of the public folder to the client as static files.
+
+
+console.log("Server is running on port 3000");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log("New Connection: " + socket.id);
+
+  socket.on('button', buttonPressed);
+  function buttonPressed(message) {
+    console.log(socket.id + " " + message);
+  }
+}
